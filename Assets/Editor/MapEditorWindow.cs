613ad9d5533a5bf44b087828bcc5394244f5b399
Lafsody <kirk.kirby@hotmail.com>
2017-05-24 14:11:53 +0700

    Create Map Editor Window

---
+++ Assets/Editor/MapEditorWindow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MapEditorWindow : EditorWindow
+{
+	[MenuItem("GameEditor/MapEditor")]
+	private static void Init()
+	{
+		var window = (MapEditorWindow)GetWindow(typeof(MapEditorWindow));
+		window.minSize = new Vector2(300, 400);
+		window.Show();
+	}
+
+	void OnEnable()
+	{
+		
+	}
+}
