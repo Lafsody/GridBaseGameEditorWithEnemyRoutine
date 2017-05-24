2e6fbb11941a8bac455c59488f780208987af5f7
Lafsody <kirk.kirby@hotmail.com>
2017-05-25 02:01:17 +0700

    Add Section in Map Editor Window

--- Assets/Editor/MapEditorWindow.cs
+++ Assets/Editor/MapEditorWindow.cs
@@ -5,6 +5,14 @@ using UnityEditor;
 
 public class MapEditorWindow : EditorWindow
 {
+	Texture2D headerTexture;
+	Texture2D visibilityControlTexture;
+	Texture2D objectEditorTexture;
+
+	Rect headerSection;
+	Rect visibilityControlSection;
+	Rect objectEditorSection;
+
 	[MenuItem("GameEditor/MapEditor")]
 	private static void Init()
 	{
@@ -15,6 +23,48 @@ public class MapEditorWindow : EditorWindow
 
 	void OnEnable()
 	{
+		InitTexture();
+	}
+
+	void InitTexture()
+	{
+		headerTexture = Resources.Load<Texture2D>("bg/bg1");
+		visibilityControlTexture = Resources.Load<Texture2D>("bg/bg2");
+		objectEditorTexture = Resources.Load<Texture2D>("bg/bg3");
+	}
+
+	void OnGUI()
+	{
+		DrawLayout();
+		DrawHeader();
+	}
+
+	private void DrawLayout()
+	{
+		headerSection.x = 0;
+		headerSection.y = 0;
+		headerSection.width = Screen.width;
+		headerSection.height = 50;
+
+		visibilityControlSection.x = 0;
+		visibilityControlSection.y = headerSection.height;
+		visibilityControlSection.width = Screen.width / 2;
+		visibilityControlSection.height = Screen.height - headerSection.height;
 		
+		objectEditorSection.x = visibilityControlSection.width;
+		objectEditorSection.y = headerSection.height;
+		objectEditorSection.width = Screen.width - visibilityControlSection.width;
+		objectEditorSection.height = Screen.height - headerSection.height;
+
+		GUI.DrawTexture(headerSection, headerTexture);
+		GUI.DrawTexture(visibilityControlSection, visibilityControlTexture);
+		GUI.DrawTexture(objectEditorSection, objectEditorTexture);
+	}
+
+	void DrawHeader()
+	{
+		GUILayout.BeginArea(headerSection);
+		GUILayout.Label("Map Editor");
+		GUILayout.EndArea();
 	}
 }
