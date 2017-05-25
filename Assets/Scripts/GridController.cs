aed1b4d0cd4e5cd78b9c4ccb1d15720d6bbe1f5b
Lafsody <kirk.kirby@hotmail.com>
2017-05-25 18:12:04 +0700

    - Attach script to grid

---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+	public bool canPlayerEnter;
+	public bool canPlayerExit;
+
+	[Space]
+
+	public bool canEnemyEnter;
+	public bool canEnemyExit;
+}
