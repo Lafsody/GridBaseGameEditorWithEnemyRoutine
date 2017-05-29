c0914155d456b3531c3b5a1ec94c0b78fbdb56bb
Lafsody <kirk.kirby@hotmail.com>
2017-05-29 13:47:09 +0700

    - Can Generate Grid by shift + a after open Grid Editor Window

--- Assets/Editor/GridEditorWindow.cs
+++ Assets/Editor/GridEditorWindow.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
+enum EditOption {Null, Add, Delete, Select}
+
 public class GridEditorWindow : EditorWindow
 {
+	private static bool isEnabled;
+	private static EditOption selected;
+
 	public static void OpenWindow()
 	{
 		var window = GetWindow(typeof(GridEditorWindow)) as GridEditorWindow;
@@ -14,6 +19,54 @@ public class GridEditorWindow : EditorWindow
 
 	void OnEnable()
 	{
+		isEnabled = true;
+		Editor.CreateInstance(typeof(SceneViewEventHandler));
+	}
+
+	void OnDestroy()
+	{
+		isEnabled = false;
+	}
+
+	void OnGUI()
+	{
+		
+	}
+
+	public class SceneViewEventHandler : Editor
+	{
+		static SceneViewEventHandler()
+		{
+			SceneView.onSceneGUIDelegate += OnSceneGUI;
+		}
+
+		static void OnSceneGUI(SceneView aView)
+		{
+			if (!isEnabled)
+				return;
+
+			Event hotkey_e = Event.current;
+			switch(hotkey_e.type)
+			{
+				case EventType.KeyDown:
+					if (hotkey_e.shift)
+					{
+						switch(hotkey_e.keyCode)
+						{
+							case KeyCode.A:
+								CreateGrid();
+								break;
+						}
+					}
+					break;
+			}
+		}
+	}
+
+	static void CreateGrid()
+	{
+		Object prefab = AssetDatabase.LoadAssetAtPath("Assets/Prefabs/SceneObjects/Floor.prefab", typeof(GameObject));
+		GameObject clone = Instantiate(prefab, Vector3.zero, Quaternion.identity) as GameObject;
 		
 	}
 }
